9aeb40079a625db4c38584da0be590045a0d42be
glitchy <lukas.r.jorgensen@gmail.com>
2022-11-10 17:05:50 -0500

    adding more opcodes from CHIP8 docs

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,37 +1,117 @@
-pub struct CPU {
-    pub registers: [u8; 16],
-    pub program_counter: usize,
+pub struct Chip8 {
     pub memory: [u8; 4096],
+    pub program_counter: usize,
+    pub registers: [u8; 16],
     pub stack: [u16; 16],
     pub stack_pointer: usize,
 }
 
-impl CPU {
+impl Chip8 {
     pub fn run(&mut self) {
         loop {
-            let opcode = self.read_opcode();
+            let op_byte1 = self.memory[self.program_counter] as u16;
+            let op_byte2 = self.memory[self.program_counter + 1] as u16;
+            let opcode: u16 = op_byte1 << 8 | op_byte2;
+
+            // ##### xykk #####
+            // let x = ((opcode & 0xF000) >>  8) as u8;
+            // let y = ((opcode & 0x0F00) >>  4) as u8;
+            // let kk = (opcode & 0x00FF) as u8;
+            // let nnn = opcode & 0x0FFF;
+
             self.program_counter += 2;
 
-            let c = ((opcode & 0xF000) >> 12) as u8;
-            let x = ((opcode & 0x0F00) >>  8) as u8;
-            let y = ((opcode & 0x00F0) >>  4) as u8;
-            let d = ((opcode & 0x000F) >>  0) as u8;
-
-            let nnn = opcode & 0x0FFF;
-            //let k = (opcode & 0x0FFF) as u8;
-
-            match (c, x, y, d) {
-                // Short-circuit fn to terminate execution when 0x0000 is encountered.
-                (  0,   0,   0,   0) => { return; },
-                (  0,   0, 0xE, 0xE) => self.ret(),
-                (0x2,   _,   _,   _) => self.call(nnn),
-                (0x8,   _,   _, 0x4) => self.add(x, y),
-                _                => todo!("opcode {:04x}", opcode),
+            match opcode {
+                0x0000 => { return; },
+                0x00E0 => { /* clear screen */ },
+                // RETURN from subroutine
+                0x00EE => { self.return(); },
+                // JUMP to address NNN
+                0x1FFF => { 
+                    self.jump(
+                        opcode & 0x0FFF // nnn
+                    ); 
+                },
+                // CALL subroutine starting at address 'nnn'
+                0x2FFF => { 
+                    self.call(
+                        opcode & 0x0FFF // nnn
+                    ); 
+                },
+                // SKIP the following instruction if the value of register[VX] is EQUAL to 'kk'
+                0x3FFF => {
+                    self.skip_if_equal(
+                        ((opcode & 0xF000) >> 8) as u8, // x
+                        (opcode & 0x00FF) as u8 // kk
+                    ); 
+                },
+                // SKIP the following instruction if the value of rVX is NOT EQUAL NN
+                0x4FFF => { 
+                    self.skip_if_not_equal(
+                        ((opcode & 0xF000) >>  8) as u8, // x
+                        (opcode & 0x00FF) as u8 // kk
+                    );
+                },
+                // SKIP the following instruction if the value of rVX is EQUAL to NN
+                0x5FFF => {
+                    self.skip_if_equal(
+                        ((opcode & 0xF000) >>  8) as u8, // x
+                        ((opcode & 0x0F00) >>  4) as u8 // y
+                    );
+                },
+                // store (LOAD) NN in register VX
+                0x6FFF => {
+                    self.load(
+                        ((opcode & 0xF000) >>  8) as u8, // x
+                        (opcode & 0x00FF) as u8 // kk
+                    );
+                },
+                0x7FFF => {
+                    self.add(
+                        ((opcode & 0xF000) >>  8) as u8, // x
+                        (opcode & 0x00FF) as u8 // kk
+                    );
+                },
+                0x8FFF => {
+                    match (opcode & 0x000F) as u8 {
+                        0 => {
+                            self.load(
+                                ((opcode & 0xF000) >>  8) as u8, // x
+                                self.registers[((opcode & 0x0F00) >>  4) as usize]) 
+                        },
+                        1 => { 
+                            self.or_xy(
+                                ((opcode & 0xF000) >>  8) as u8, // x
+                                ((opcode & 0x0F00) >>  4) as u8, // y
+                            ) 
+                        },
+                        2 => {
+                            self.and_xy(
+                                ((opcode & 0xF000) >>  8) as u8, // x
+                                ((opcode & 0x0F00) >>  4) as u8, // y
+                            ) 
+                        },
+                        3 => {
+                            self.xor_xy(
+                                ((opcode & 0xF000) >>  8) as u8, // x
+                                ((opcode & 0x0F00) >>  4) as u8, // y
+                            ) 
+                        },
+                        4 => { 
+                            self.add_xy(
+                                ((opcode & 0xF000) >>  8) as u8, // x
+                                ((opcode & 0x0F00) >>  4) as u8, // y
+                            ) 
+                        },
+                        _ => { todo!("opcode: {:04x}", opcode); },
+                    }
+                },
+                _ => { todo!("opcode: {:04x}", opcode); },
             }
         }
     }
 
-    fn add(&mut self, x: u8, y: u8) {
+    fn add_xy(&mut self, x: u8, y: u8) {
         let arg1 = self.registers[x as usize];
         let arg2 = self.registers[y as usize];
 
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 pub mod cpu;
 
-use cpu::CPU;
+use cpu::Chip8;
 
 fn main() {
-    let mut cpu = CPU {
-        registers: [0; 16],
-        program_counter: 0,
+    let mut cpu = Chip8 {
         memory: [0; 4096],
+        program_counter: 0,
+        registers: [0; 16],
         stack: [0; 16],
         stack_pointer: 0,
     };
@@ -20,18 +20,23 @@ fn main() {
     //   |/\|    |/\|    |/\|    |/\|    |/\|    |/\|
     //   |  |    |  |    |  |    |  |    |  |    |  |
     //  [0][1]  [2][3]  [4][5]  [6][7]  [8][9] [10][11] 
+    //
+    // ##### opcode definitions #####
+    // 0x2100 = CALL fn at 0x100
     mem[0x000] = 0x21; mem[0x001] = 0x00;
+    // 0x2100 = CALL fn at 0x100
     mem[0x002] = 0x21; mem[0x003] = 0x00;
+    // 0x0000 = HALT
     mem[0x004] = 0x00; mem[0x005] = 0x00;
-
+    // 0x8014 = ADD r1 to r0
     mem[0x100] = 0x80; mem[0x101] = 0x14;
+    // 0x8014 = ADD r1 to r0
     mem[0x102] = 0x80; mem[0x103] = 0x14;
+    // 0x00EE = RETURN
     mem[0x104] = 0x00; mem[0x105] = 0xEE;
-
 
     cpu.run();
 
     assert_eq!(cpu.registers[0], 45);
-
     println!("5 + (10 * 2) + (10 * 2) = {}", cpu.registers[0]);
-}
+    }
